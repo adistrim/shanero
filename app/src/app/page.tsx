38a8dcb45bb4ab4b346bedadc1ee16ea641fdc9e
adistrim <adistrim.dev@zohomail.in>
2025-11-12 23:54:47 +0530

    chore: react -> next

---
+++ app/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <h1 className="font-stretch-200% text-4xl">Shanero</h1>
+    </div>
+  );
+}
