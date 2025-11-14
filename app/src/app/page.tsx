8a5d99cff6608c75c49a6599e45ba344c0abb278
adistrim <adistrim.dev@zohomail.in>
2025-11-14 17:21:06 +0530

    feat: add shadcn, and next-themes

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <h1 className="font-stretch-200% text-4xl">Shanero</h1>
+    <div className="flex flex-col min-h-screen items-center justify-center">
+      <h1 className="font-stretch-120% text-4xl">Shanero is a slow social platform.</h1>
+      <h2 className="font-light text-lg">We value reflection over reaction.</h2>
+      <Button className="font-stretch-200%">I&apos;m in</Button>
     </div>
   );
 }
